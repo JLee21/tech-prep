603550601743756a31114c34c0a0eb4e7edf05c5
Jordan <leejchris21@gmail.com>
2019-10-23 16:46:26 -0700

    add complete inorder, preorder, postorder drills with tests
    Signed-off-by: Jordan <leejchris21@gmail.com>

--- code-problems/drills.one.py
+++ code-problems/drills.one.py
@@ -2,79 +2,135 @@ from trees import root
 
 """
 Given a root node to a binary tree... 
-    print out the values using DFS - recursively
-    print out the values using DFS - iteratively
-    print out the values using BFS
-"""
-
-
-def search(root):
-    if not root:
-        return
-    print(root.val)
-    search(root.left)
-    search(root.right)
-
-
-search(root)
-
-
-def search(root):
-    while root:
-        print(root.value)
-        root.left
-
-
-"""
-Given a root node to a binary tree... 
     return the values Preorder - recursively - [1, 2, 5, 6, 3, 7, 8]
     return the values Preorder - iteratively
     return the values Inorder - recursively - [5, 2, 6, 1, 7, 3, 8]
     return the values Inorder - iteratively
-    return the values Postorder
+    return the values Postorder - recursively - [5, 6, 2, 7, 8, 3, 1]
+    return the values Postorder - iteratively - 
 """
 
 
 def solution_pre_rec(root):
 
     def search(root, stack):
-        if not root:
-            return False
-        stack.append(root.val)
-        return search(root.left, stack) or search(root.right, stack)
+        if root:
+            stack.append(root.val)
+            search(root.left, stack)
+            search(root.right, stack)
+        # if not root:
+        #     return False
+        # stack.append(root.val)
+        # return search(root.left, stack) or search(root.right, stack)
 
     stack = []
     if root:
         stack.append(root.val)
+    else:
+        return []
     search(root.left, stack)
     search(root.right, stack)
-    print(stack)
     return stack
 
 
 def solution_pre_iter(root):
-    pass
+    output, nodes = [], []
+
+    if root:
+        nodes.append(root)
+    else:
+        return []
+
+    while nodes:
+        node = nodes.pop()
+        output.append(node.val)
+
+        if node.right:
+            nodes.append(node.right)
+        if node.left:
+            nodes.append(node.left)
+
+    return output
 
 
 def solution_in_rec(root):
-    pass
+
+    def traverse(root, output):
+        if root.left:
+            traverse(root.left, output)
+        output.append(root.val)
+        if root.right:
+            traverse(root.right, output)
+
+    if not root:
+        return []
+    output = []
+    traverse(root, output)
+    return output
 
 
 def solution_in_iter(root):
-    pass
+    output, nodes = [], []
+
+    # Version 2 - avoids three nestings
+    while root or nodes:
+        while root:
+            nodes.append(root)
+            root = root.left
+        node = nodes.pop()
+        output.append(node.val)
+        root = node.right
+    return output
+
+    # Version 1
+    while nodes:
+        node = nodes[-1]
+
+        if node.left:
+            nodes.append(node.left)
+        else:
+            _node = nodes.pop()
+            output.append(_node.val)
+            if nodes:
+                _node = nodes.pop()
+                output.append(_node.val)
+                if _node.right:
+                    nodes.append(_node.right)
+    return output
 
 
-def search(root, stack):
+def solution_post_rec(root):
+
+    def traverse(root, output):
+        if root:
+            traverse(root.left, output)
+            traverse(root.right, output)
+            output.append(root.val)
+
+    output = []
+    traverse(root, output)
+    return output
+
+
+def solution_post_iter(root):
+    # Version 2
+    output, nodes = [], root and [root]
+    while nodes:
+        node = nodes.pop()
+        output.append(node.val)
+        nodes += [child for child in (node.left, node.right) if child]
+    return output[::-1]
+
+    # Version 1
     if not root:
-        return
-    if root.left:
-        search(root.left, stack)
-    stack.append(root.val)
-    if root.right:
-        search(root.right, stack)
-    return stack
+        return []
+    nodes, output = [root, ], []
 
+    while nodes:
+        node = nodes.pop()
+        if node:
+            output.append(node.val)
+            nodes.append(node.left)
+            nodes.append(node.right)
 
-stack = []
-ans = search(root, stack)
-print(ans)
+    return output[::-1]
--- code-problems/test_drills.py
+++ code-problems/test_drills.py
@@ -13,15 +13,29 @@ Binary Tree
 
 def test_pre_rec():
     assert drills.solution_pre_rec(root) == [1, 2, 5, 6, 3, 7, 8]
+    assert drills.solution_pre_rec(None) == []
 
 
 def test_pre_iter():
     assert drills.solution_pre_iter(root) == [1, 2, 5, 6, 3, 7, 8]
+    assert drills.solution_pre_iter(None) == []
 
 
 def test_in_rec():
     assert drills.solution_in_rec(root) == [5, 2, 6, 1, 7, 3, 8]
+    assert drills.solution_in_rec(None) == []
 
 
 def test_in_iter():
     assert drills.solution_in_iter(root) == [5, 2, 6, 1, 7, 3, 8]
+    assert drills.solution_in_iter(None) == []
+
+
+def test_post_rec():
+    assert drills.solution_post_rec(root) == [5, 6, 2, 7, 8, 3, 1]
+    assert drills.solution_post_rec(None) == []
+
+
+def test_post_iter():
+    assert drills.solution_post_iter(root) == [5, 6, 2, 7, 8, 3, 1]
+    assert drills.solution_post_iter(None) == []
